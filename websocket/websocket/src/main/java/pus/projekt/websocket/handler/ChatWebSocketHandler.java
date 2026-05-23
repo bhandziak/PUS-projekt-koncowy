cf9dd70ec59c71e917f9cff9a84e8cff53978437
Aneiv <161586104+Aneiv@users.noreply.github.com>
2026-05-23 15:30:51 +0200

    Add handshake response

--- websocket/websocket/src/main/java/pus/projekt/websocket/handler/ChatWebSocketHandler.java
+++ websocket/websocket/src/main/java/pus/projekt/websocket/handler/ChatWebSocketHandler.java
@@ -5,8 +5,19 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import pus.projekt.websocket.dto.Meta;
+import pus.projekt.websocket.dto.Request;
+import pus.projekt.websocket.dto.Response;
+import pus.projekt.websocket.enums.ErrorCode;
+import pus.projekt.websocket.enums.Status;
+import pus.projekt.websocket.enums.Type;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class ChatWebSocketHandler extends TextWebSocketHandler {
@@ -30,7 +41,36 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
 
             switch (type) {
                 case "HANDSHAKE":
-                    // TODO return handshake json
+                    // Map payload to DTO request
+                    Request clientRequest = objectMapper.treeToValue(rootNode, Request.class);
+
+                    // get client action and handle invalid data (action/payload)
+                    if (clientRequest.payload() == null || !clientRequest.payload().action().equals("hello")) {
+                            Meta errorMeta = new Meta("1.0.0", UUID.randomUUID(), LocalDateTime.now());
+                            Response validationError = new Response(
+                                    Type.ERROR,
+                                    Status.FAIL,
+                                    null,
+                                    new Response.ErrorDTO(ErrorCode.VALIDATION_ERROR, "sent payload is incorrect"),
+                                    errorMeta
+                            );
+                            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(validationError)));
+                            break;
+                        }
+                    // response build
+                    Meta successMeta = new Meta(
+                            "1.0.0",
+                            UUID.randomUUID(),
+                            LocalDateTime.now()
+                    );
+                    Map<String, Object> responseData = Map.of("message", "Witaj na serwerze IRC");
+
+                    // response object
+                    Response handshakeAck = Response.success(Type.HANDSHAKE, "hello", responseData, successMeta);
+
+                    // convert to json and send
+                    String jsonResponse = objectMapper.writeValueAsString(handshakeAck);
+                    session.sendMessage(new TextMessage(jsonResponse));
                     break;
                 default:
                     System.out.println("Request: Unknown type");
