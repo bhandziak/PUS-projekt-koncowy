763560079bd4a9862d47f4fdc952eaea6c8a357d
GaskaPiotr <04piotr04@wp.pl>
2026-05-27 09:27:11 +0200

    fet: add jwt access and refresh token

---
+++ websocket/websocket/src/main/java/pus/projekt/websocket/service/JwtService.java
@@ -0,0 +1,43 @@
+package pus.projekt.websocket.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+import pus.projekt.websocket.model.User;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY_STRING = "MojaSuperTajnaiBardzoDlugaZmiennaDoPodpisywaniaTokenowJWT12345!";
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY_STRING.getBytes());
+
+    // 15 min w milisekundach
+    private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15;
+
+    // 7 dni w milisekundach
+    private static final long REFRESH_TOKEN_EXPIRATION = 1000 * 60 * 60 * 24 * 7;
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .subject(user.getId().toString())
+                .claim("username", user.getUsername())
+                .claim("role", user.getRole())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+
+    public String generateRefreshToken(User user) {
+        return Jwts.builder()
+                .subject(user.getId().toString())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+}
+
+
