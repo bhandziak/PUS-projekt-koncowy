99dd278299bb8f767b21dfa5827c87b8375cc65a
Aneiv <161586104+Aneiv@users.noreply.github.com>
2026-05-28 12:09:06 +0200

    add enum for user role
    +add enum for user role
    +modified model User to use UserRole enum
    +add ADMIN user to database_test_data.sql

--- websocket/websocket/src/main/java/pus/projekt/websocket/model/User.java
+++ websocket/websocket/src/main/java/pus/projekt/websocket/model/User.java
@@ -25,7 +25,8 @@ public class User {
     private String password;
 
     @Column(nullable = false)
-    private String role = "USER";
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
 
     public User(String username, String password) {
         this.username = username;
--- websocket/websocket/src/main/resources/sql/database_test_data.sql
+++ websocket/websocket/src/main/resources/sql/database_test_data.sql
@@ -9,4 +9,5 @@
 
 
 INSERT INTO users (id, username, password, role) VALUES
-(gen_random_uuid(),'andrzej', '$2a$12$sDlIkYyL34rzFB78q.1Ls.ZAalBNn.oyrN1mIHMjqBee4ZTKNOp22', 'USER');
+(gen_random_uuid(),'andrzej', '$2a$12$sDlIkYyL34rzFB78q.1Ls.ZAalBNn.oyrN1mIHMjqBee4ZTKNOp22', 'USER'),
+(gen_random_uuid(),'jan', '$2a$12$sDlIkYyL34rzFB78q.1Ls.ZAalBNn.oyrN1mIHMjqBee4ZTKNOp22', 'ADMIN');
