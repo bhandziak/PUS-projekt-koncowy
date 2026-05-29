e963b342eced98e2db8a48cc7d12e0e0f0254bdf
Aneiv <161586104+Aneiv@users.noreply.github.com>
2026-05-29 14:51:37 +0200

    changed payload data to object #39
    +changed payload data to object
    +changed messagehandler logic to handle success and error messages

--- websocket/websocket/src/main/java/pus/projekt/websocket/dto/Payload.java
+++ websocket/websocket/src/main/java/pus/projekt/websocket/dto/Payload.java
@@ -6,6 +6,5 @@ import java.util.Map;
 public record Payload(
         // TODO moze lepszy enum
         String action,
-        // TODO moze lepszy inny typ niz List<String>
-        Map<String, Object> data
+        Object data
 ) {}
--- websocket/websocket/src/main/java/pus/projekt/websocket/handler/MessageHandler.java
+++ websocket/websocket/src/main/java/pus/projekt/websocket/handler/MessageHandler.java
@@ -1,12 +1,33 @@
 package pus.projekt.websocket.handler;
 
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+import pus.projekt.websocket.config.TimestampConverter;
+import pus.projekt.websocket.dto.Meta;
+import pus.projekt.websocket.dto.Payload;
 import pus.projekt.websocket.dto.Request;
+import pus.projekt.websocket.dto.Response;
+import pus.projekt.websocket.enums.ErrorCode;
 import pus.projekt.websocket.enums.Type;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
 public interface MessageHandler {
     Type getSupportedType();
     void handle(WebSocketSession session, Request request) throws IOException;
+    ObjectMapper getObjectMapper();
+
+    // Default success send
+    default void sendSuccess(WebSocketSession session, String action, Object responseData, Meta meta) throws IOException {
+        Meta successMeta = new Meta("1.0.0", meta.packet_id(), TimestampConverter.currentToSeconds());
+        Response successResponse = Response.success(getSupportedType(), action, responseData, successMeta);
+        session.sendMessage(new TextMessage(getObjectMapper().writeValueAsString(successResponse)));
+    }
+
+    // Default error send
+    default void sendError(WebSocketSession session, Payload originalPayload, ErrorCode code, String message, Meta meta) throws IOException {
+        Response errorResponse = Response.error(getSupportedType(), originalPayload, code, message, meta);
+        session.sendMessage(new TextMessage(getObjectMapper().writeValueAsString(errorResponse)));
+    }
 }
