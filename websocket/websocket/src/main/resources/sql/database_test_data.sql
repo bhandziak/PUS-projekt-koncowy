e873d51c0cb7b422c4f4b43a74d9200bd6b1cc2c
Aneiv <161586104+Aneiv@users.noreply.github.com>
2026-06-03 00:18:00 +0200

    change db logic
    change db logic to not delete records on restart of backend

--- websocket/websocket/src/main/resources/sql/database_test_data.sql
+++ websocket/websocket/src/main/resources/sql/database_test_data.sql
@@ -10,4 +10,6 @@
 
 INSERT INTO users (id, username, password, role) VALUES
 (gen_random_uuid(),'andrzej', '$2a$12$sDlIkYyL34rzFB78q.1Ls.ZAalBNn.oyrN1mIHMjqBee4ZTKNOp22', 'USER'),
-(gen_random_uuid(),'jan', '$2a$12$sDlIkYyL34rzFB78q.1Ls.ZAalBNn.oyrN1mIHMjqBee4ZTKNOp22', 'ADMIN');
+(gen_random_uuid(),'jan', '$2a$12$sDlIkYyL34rzFB78q.1Ls.ZAalBNn.oyrN1mIHMjqBee4ZTKNOp22', 'ADMIN')
+ON CONFLICT (username)
+DO NOTHING;
